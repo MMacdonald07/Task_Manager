74b2553d30922e92734ab3e83ac1dc6cb779be22
mdmac <matthew.mmacdonald@gmail.com>
2020-09-16 16:28:10 +0100

    Added find methods

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,30 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
-const connectionURL = ''
-const database = ''
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
 
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('tasks').findOne({ _id: new ObjectID("5f621de0a329872f24da1553") }, (error, task) => {
+        if (error) {
+            console.log('Unable to fetch')
+        }
+
+        console.log(task)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        if (error) {
+            console.log('Unable to fetch data')
+        }
+
+        console.log(tasks)
+    })
+})
