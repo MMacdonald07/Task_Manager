2ed6eaef00b8811f1b55e22cab42135aa67c4596
mdmac <matthew.mmacdonald@gmail.com>
2020-09-19 16:38:54 +0100

    Added PATCH/DELETE requests

--- src/index.js
+++ src/index.js
@@ -8,66 +8,154 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
-        res.send(user)
-    }).catch((error) => {
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.status(202).send(users)
-    }).catch((error) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+    
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send({ error: 'Invalid ID!' })
         }
 
         res.send(user)
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 })
 
-app.post('/tasks', (req, res) => {
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates! Either one of these entries does not exist or they cannot be changed!' })
+    }
+    
+    const _id = req.params.id
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            res.status(404).send({ error: 'Invalid ID!' })
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    
+    try {
+        const user = await User.findByIdAndDelete(_id)
+
+        if (!user) {
+            return res.status(404).send({ error: 'Invalid ID!' })
+        }
+
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.status(202).send(tasks)
-    }).catch((error) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send({ error: 'Invalid ID! '})
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates! Either one of these entries does not exist or they cannot be changed!' })
+    }
+    
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            res.status(404).send({ error: 'Invalid ID!' })
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+
         if (!task) {
             return res.status(404).send({ error: 'Invalid ID!' })
         }
 
-        res.send(task)
-    }).catch((error) => {
+        res.status(200).send(task)
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 })
 
 app.listen(port, () => {
